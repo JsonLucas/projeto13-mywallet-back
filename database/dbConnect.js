e0cf58225b4935f54c43c173b4c3f44d52ad7d18
Jason <jasonlucas443@gmail.com>
2022-05-09 16:30:55 -0300

    chore: adjust code for deploy on heroku

--- database/dbConnect.js
+++ database/dbConnect.js
@@ -1,8 +1,9 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
-import { dbUri } from "../dot-env.js";
 import mongoose from "mongoose";
+import 'dotenv/config.js';
 
 const dbConnection = () => {
+    const dbUri = process.env.DB_URI;
     try{
         mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
         const db = mongoose.connection.on('error', (e) => {
--- dot-env.js
+++ dot-env.js
@@ -1,4 +1,4 @@
 import 'dotenv/config.js';
 
-export const serverPort = process.env.SERVER_PORT;
+export const serverPort = process.env.SERVER_PORT || 5000;
 export const dbUri = process.env.DB_URI; 
